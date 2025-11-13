f3a7e208ffb60deac8e14a88af5a02aebb378de9
Jonathan <41653402+jvc5546@users.noreply.github.com>
2025-11-13 15:43:32 -0500

    Only append 'okteto' namespace instead of rewritting code

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/okteto-community/list-pod-nodes/api"
+	"github.com/okteto-community/list-pod-nodes/model"
 )
 
 func main() {
@@ -52,40 +53,18 @@ func main() {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
+	// Append okteto namespace to the beginning of the list of namespaces
+	oktetoNamespace := model.Namespace{
+		Name: "okteto",
+		Type: "okteto",
+	}
+	nsList = append([]model.Namespace{oktetoNamespace}, nsList...)
+
 	// Write headers
 	headers := []string{"Pod Name", "Namespace", "Namespace Type", "Node Name"}
 	if err := writer.Write(headers); err != nil {
 		logger.Error(fmt.Sprintf("Error writing headers to CSV: %s", err))
 		os.Exit(1)
-	}
-
-	// Write data rows for Okteto namespace
-	cmdStr := `kubectl get pod -n okteto -o json | jq -r '.items[] | "\(.metadata.name)\t\(.metadata.namespace)\t\(.spec.nodeName)"'`
-	cmd := exec.Command("bash", "-c", cmdStr)
-	output, err := cmd.CombinedOutput()
-
-	if err != nil {
-		fmt.Printf("Error executing command: %v\n", err)
-		return
-	}
-
-	outputStr := strings.TrimSpace(string(output))
-	if outputStr != "" {
-		lines := strings.Split(string(outputStr), "\n")
-		for _, line := range lines {
-			parts := strings.Split(line, "\t")
-			podName := parts[0]
-			namespaceName := parts[1]
-			nodeName := parts[2]
-
-			// Write the row to the CSV file
-			row := []string{podName, namespaceName, "okteto", nodeName}
-			if err := writer.Write(row); err != nil {
-				logger.Error(fmt.Sprintf("Error writing row to CSV: %s", err))
-				os.Exit(1)
-			}
-			fmt.Println(line)
-		}
 	}
 
 	// Write data rows for Preview, Development, and Agent namespaces
