a086d90e279e61a35aa29d051ec01e51795819e6
Jonathan <41653402+jvc5546@users.noreply.github.com>
2025-11-13 14:58:52 -0500

    Remove type filter on namespaces

--- api/namespaces.go
+++ api/namespaces.go
@@ -7,13 +7,13 @@ import (
 	"github.com/okteto-community/list-pod-nodes/model"
 )
 
-const (
-	developmentNamespaceType = "development"
-)
+// const (
+// 	developmentNamespaceType = "development"
+// )
 
 // GetNamespaces retrieves all the namespaces
 func GetDevelopmentNamespaces(baseURL, token string, logger *slog.Logger) ([]model.Namespace, error) {
-	namespacesURL := fmt.Sprintf("https://%s/%s?type=%s", baseURL, namespacesAPIPath, developmentNamespaceType)
+	namespacesURL := fmt.Sprintf("https://%s/%s", baseURL, namespacesAPIPath)
 	var namespaces []model.Namespace
 	if err := sendRequest(namespacesURL, token, &namespaces, logger); err != nil {
 		return nil, err
